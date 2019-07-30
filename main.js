aa63ded8274906b335983bf1935a654715b00935
Ana Amaro <amarovazquezana@gmail.com>
2019-07-30 18:52:52 +0200

    change main.js-fase/3

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function ten(number, word) {
+function writeThis(number, word) {
     for (let i = 0; i <= number; i++) {
         console.log(i, word);
     }
 }
 
-ten(3, 'aguacate');
+writeThis(3, 'aguacate');
